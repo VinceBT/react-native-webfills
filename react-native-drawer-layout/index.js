b3b2f2553e986415b6bf01398708aa89d3547424
Unknown <vincebt06@gmail.com>
2017-01-30 21:26:40 +0100

    DrawerLayout implemented

--- react-native-drawer-layout/index.js
+++ react-native-drawer-layout/index.js
@@ -1,62 +1,87 @@
 // @flow
-import React, { Component } from 'react';
-import { Animated, View } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { Animated, View, TouchableWithoutFeedback } from 'react-native';
 
 export default class DrawerLayout extends Component {
 
   static defaultProps = {
+    drawerBackgroundColor: 'white',
     drawerWidth: 300,
-    animationDuration: 300,
   };
 
   static propTypes = {
-    drawerWidth: React.PropTypes.number,
-    animationDuration: React.PropTypes.number,
-    renderNavigationView: React.PropTypes.func.isRequired,
-    children: React.PropTypes.element,
+    ...View.propTypes,
+    drawerBackgroundColor: PropTypes.any,
+    drawerPosition: PropTypes.oneOf([]),
+    drawerWidth: PropTypes.number,
+    drawerLockMode: PropTypes.oneOf([
+      'unlocked',
+      'locked-closed',
+      'locked-open',
+    ]),
+    onDrawerOpen: PropTypes.func,
+    onDrawerClose: PropTypes.func,
+    renderNavigationView: PropTypes.func.isRequired,
   };
 
-  constructor(props: Object) {
-    super(props);
-    this._closed = false;
-    this._animatedWidth = new Animated.Value(this.props.drawerWidth);
-  }
+  state = {
+    open: false,
+  };
 
-  _closed: boolean;
-  _animatedWidth: Animated.Value;
+  _animValue: Animated.Value = new Animated.Value(0);
 
   openDrawer = () => {
-    if (this._closed) {
-      this._closed = false;
-      Animated.timing(this._animatedWidth, {
-        toValue: this.props.drawerWidth,
-        duration: this.props.animationDuration,
-      }).start();
+    if (!this.state.open) {
+      this.setState({ open: true });
+      Animated.timing(this._animValue, { toValue: 1, duration: 200 }).start();
     }
   };
 
   closeDrawer = () => {
-    if (!this._closed) {
-      this._closed = true;
-      Animated.timing(this._animatedWidth, {
-        toValue: 0,
-        duration: this.props.animationDuration,
-      }).start();
+    if (this.state.open) {
+      Animated.timing(this._animValue, { toValue: 0, duration: 200 }).start(() => {
+        this.setState({ open: false });
+      });
     }
   };
 
   render() {
+    const { renderNavigationView, drawerWidth, drawerBackgroundColor, style, children } = this.props;
     return (
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <Animated.View
-          style={{ width: this._animatedWidth, overflow: 'hidden', flexDirection: 'row', justifyContent: 'flex-end' }}>
-          <View style={{ width: this.props.drawerWidth }}>
-            {this.props.renderNavigationView()}
-          </View>
-        </Animated.View>
+      <View style={[style, { position: 'relative', overflow: 'hidden' }]}>
         <View style={{ flex: 1 }}>
-          {this.props.children}
+          {children}
         </View>
+        {this.state.open && (
+          <TouchableWithoutFeedback onPress={() => this.closeDrawer()}>
+            <Animated.View
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundColor: 'rgba(0, 0, 0, .65)',
+                opacity: this._animValue }} />
+          </TouchableWithoutFeedback>
+        )}
+        {this.state.open && (
+          <Animated.View
+            style={{
+              backgroundColor: drawerBackgroundColor,
+              position: 'absolute',
+              top: 0,
+              bottom: 0,
+              width: drawerWidth,
+              boxShadow: '0 0 10px 5px rgba(0, 0, 0, .8)',
+              left: this._animValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [-drawerWidth, 0],
+              }),
+            }}>
+            {renderNavigationView()}
+          </Animated.View>
+        )}
       </View>
     );
   }
