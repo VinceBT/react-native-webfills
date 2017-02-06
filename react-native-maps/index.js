ed38d4df9f1c3778d955dbbedb3af89984eb2d3c
Unknown <vincebt06@gmail.com>
2017-02-06 21:45:26 +0100

    Fixed a bug is there was not an InitialRegion for MapView

--- react-native-maps/index.js
+++ react-native-maps/index.js
@@ -13,16 +13,20 @@ export default class MapView extends Component {
     const { initialRegion } = this.props;
     const mapOptions = {};
     if (initialRegion) {
-      mapOptions.center = { lat: initialRegion.lat, lng: initialRegion.lng };
+      mapOptions.center = { lat: initialRegion.latitude, lng: initialRegion.longitude };
       mapOptions.zoom = 8;
+    } else {
+      mapOptions.center = { lat: 0, lng: 0 };
+      mapOptions.zoom = 2;
     }
+    console.log(this.props)
     const map = new google.maps.Map(domNode, mapOptions);
   }
 
   _mainView: ?View = null;
 
   render() {
-    const { style, ...otherProps } = this.props;
+    const { initialRegion, style, ...otherProps } = this.props;
     return (
       <View
         ref={c => { this._mainView = c; }}
