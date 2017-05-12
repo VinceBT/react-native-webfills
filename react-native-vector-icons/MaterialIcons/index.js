ea03fdc7f88a29d59eae7117d0357aa8e82ee192
VinceBT <vincebt06@gmail.com>
2017-05-12 16:09:12 +0200

    Added help to MaterialIcon usage

--- react-native-vector-icons/MaterialIcons/index.js
+++ react-native-vector-icons/MaterialIcons/index.js
@@ -1,9 +1,14 @@
 // @flow
-import React from "react";
 
-export default ({name = 'android-arrow-back', color = '#000', size = 24, style = {}}) => {
+import React from 'react';
+
+// Add <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> to the <head />
+
+const MaterialIcon = ({ name = 'android-arrow-back', color = '#000', size = 24, style = {} }) => {
   const actualName = name.replace('android-', '').split('-').join('_');
   return (
     <i className="material-icons" style={{ fontSize: size, color, ...style }}>{actualName}</i>
   );
 };
+
+export default MaterialIcon;
