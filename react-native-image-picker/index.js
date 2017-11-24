d93b1ec9f0d3a74a3afead59098f43ad2a49e126
Unknown <vincebt06@gmail.com>
2017-11-25 00:30:04 +0100

    Added default image upload to ImagePicker

--- react-native-image-picker/index.js
+++ react-native-image-picker/index.js
@@ -15,12 +15,40 @@ const DEFAULT_OPTIONS = {
   },
 };
 
+function uint8ToString(buf) {
+  let i,
+    length,
+    out = '';
+  for (i = 0, length = buf.length; i < length; i += 1)
+    out += String.fromCharCode(buf[i]);
+
+  return out;
+}
+
 const ImagePicker = {
   showImagePicker(options, callback) {
     const actualCallback = typeof options === 'function' ? options : callback;
     const actualOptions = typeof options === 'function' ? {} : options;
-    console.log({...DEFAULT_OPTIONS, ...options});
-    // navigator.mediaDevices.getUserMedia() 
+    console.log({ ...DEFAULT_OPTIONS, ...options });
+    // navigator.mediaDevices.getUserMedia()
+    const fileElem = document.createElement('input');
+    fileElem.type = 'file';
+    fileElem.accept = 'image/*';
+    fileElem.style = 'display:none';
+    fileElem.onchange = function () {
+      const file = this.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        const base64StringWithPrefix = reader.result;
+        const base64String = base64StringWithPrefix.slice(base64StringWithPrefix.indexOf(',') + 1);
+        actualCallback({ data: base64String });
+      };
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+    };
+    fileElem.click();
   },
 };
 
