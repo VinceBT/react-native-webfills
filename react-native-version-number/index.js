f12d90a90877a25412f7544eda84d7e4dd628dee
Unknown <vincebt06@gmail.com>
2017-12-05 23:54:10 +0100

    Quick fix

--- react-native-version-number/index.js
+++ react-native-version-number/index.js
@@ -2,8 +2,13 @@
 // <meta name="version" content="1.0">
 // <meta name="build" content="1">
 
-export const appVersion = (document && document.querySelector && document.querySelector('meta[name=version]')) || '';
-export const buildVersion = (document && document.querySelector && document.querySelector('meta[name=build]')) || '';
+const versionTag = (document && document.querySelector && document.querySelector('meta[name=version]')) || null;
+const buildTag = (document && document.querySelector && document.querySelector('meta[name=build]')) || null;
+
+console.log(versionTag, buildTag)
+
+export const appVersion = (versionTag && versionTag.getAttribute('content')) || '';
+export const buildVersion = (buildTag && buildTag.getAttribute('content')) || '';
 
 const VersionNumber = {
   appVersion,
